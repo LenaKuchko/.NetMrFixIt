07d634f6782b6d4d30226ad84692b9d42acf944d
Olena Kuchko <kuchkoel@gmail.com>
2017-08-18 11:32:41 -0700

    fix bug with repetitivness

--- src/MrFixIt/Controllers/JobsController.cs
+++ src/MrFixIt/Controllers/JobsController.cs
@@ -47,5 +47,11 @@ namespace MrFixIt.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult MarkComplete(int id)
+        {
+            var thisItem = db.Jobs.FirstOrDefault(item => item.JobId == id);
+            return View(thisItem);
+        }
     }
 }
